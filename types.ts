78bc07fcda3c54094a6f5c5a6d2680152ec9b070
Jang Haemin <io@jhaemin.com>
2021-10-05 00:21:20 +0900

    Develop DeepOmitType and the new Diff type

--- types.ts
+++ types.ts
@@ -99,20 +99,16 @@ type UniqueKey<Key extends string, Value = any> = {
 /**
  * Exclude nested
  */
-type Difference<A, B> = {
+type DifferenceWithNever<A, B> = {
   [K in keyof A]: K extends keyof B
     ? A[K] extends object
       ? B[K] extends object
-        ? Difference<A[K], B[K]>
+        ? DifferenceWithNever<A[K], B[K]>
         : A[K]
       : A[K] extends B[K]
       ? never
       : A[K]
     : A[K]
-}
-
-type Diff<A, B> = {
-  [K in Exclude<keyof A, 'hello'>]: A[K]
 }
 
 type DDD = {
@@ -134,28 +130,84 @@ type ND = {
 //     : Record<T, OT>
 //   : never : never : never
 
-type ExtractOnlyTheKeysOfSpecificType<
-  Obj extends object,
-  T
-> = keyof Obj extends infer Key
-  ? Key extends keyof Obj
-    ? Obj[Key] extends T
-      ? never
-      : Key
+/**
+ * Extract a union of key string with the given type.
+ * ex)
+ * ExtractKeyOfType<{
+ *   hello: string
+ *   world: number
+ *   bye: string
+ * }, string>
+ * -> 'hello' | 'bye'
+ */
+type ExtractKeyOfType<Obj, T> = Obj extends object
+  ? keyof Obj extends infer Key
+    ? Key extends keyof Obj
+      ? Obj[Key] extends T
+        ? Key extends string
+          ? Key
+          : never
+        : never
+      : never
     : never
   : never
 
+/**
+ * Opposite of ExtractKeyOfType.
+ * ex)
+ * ExcludeKeyOfType<{
+ *   hello: string
+ *   world: number
+ *   bye: string
+ * }, string>
+ * -> 'world'
+ */
+type ExcludeKeyOfType<Obj, T> = Obj extends object
+  ? keyof Obj extends infer Key
+    ? Key extends keyof Obj
+      ? Obj[Key] extends T
+        ? never
+        : Key extends string
+        ? Key
+        : never
+      : never
+    : never
+  : never
+
+/**
+ * Make a sub-record with the given union of keys.
+ * ex)
+ * SubObject<{ hello: string, world: number }, 'hello'> -> { hello: string }
+ */
 type SubObject<Obj extends object, Key extends keyof Obj> = {
   [K in Key]: Obj[K]
 }
 
-type OmitType<Obj extends object, T> = SubObject<
-  Obj,
-  ExtractOnlyTheKeysOfSpecificType<Obj, T>
->
+/**
+ * Omit object properties with the given type.
+ * ex)
+ * OmitType<{ hello: string, world: number }, string> -> { world: number }
+ */
+type OmitType<Obj extends object, T> = SubObject<Obj, ExcludeKeyOfType<Obj, T>>
+
+/**
+ * Nested version of OmitType.
+ */
+type DeepOmitType<Obj, T, OmittedObj = Omit<Obj, ExtractKeyOfType<Obj, T>>> = {
+  [K in keyof OmittedObj]: OmittedObj[K] extends object
+    ? DeepOmitType<OmittedObj[K], T>
+    : OmittedObj[K]
+}
+
+/**
+ * Get difference object type between two nested object types.
+ */
+type Diff<A, B> = DeepOmitType<DifferenceWithNever<A, B>, never>
 
 type A = {
   hello: string
+  good?: string
+  okay?: number
   world?: {
     my: number
     name: number
@@ -164,17 +216,18 @@ type A = {
 }
 
 type B = {
-  hello: string
-  world: {
+  good?: string
+  world?: {
     is: number
   }
 }
 
-const diff: OmitType<Difference<A, B>, never> = {}
+const a: DeepOmitType<A, string> = {}
 
-// const diff: DeepOmit<A, { hello: never }> = {
-//   world: {
-//     my: 3,
-//     name: 3,
-//   },
-// }
+const diff: Diff<A, B> = {
+  hello: '',
+  world: {
+    my: 1,
+    name: 2,
+  },
+}
